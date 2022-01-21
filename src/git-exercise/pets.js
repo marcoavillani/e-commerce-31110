28ceef92d7bb86cc982360011dba9f76bdc81f3c
marcoavillani <villanimarcoa@gmail.com>
2022-01-21 17:01:44 -0300

    pets added

---
+++ src/git-exercise/pets.js
@@ -0,0 +1,7 @@
+const pets = [
+  { animalType: "dog", name: "Max", age: "2" },
+  { animalType: "cat", name: "Quitilipi", age: "4" },
+  { animalType: "turtle", name: "Manuelita", age: "7" },
+  { animalType: "lion", name: "Marco", age: "34" },
+  { animalType: "zebra", name: "Blue", age: "21" },
+];
